118e5f51e082a4078703a365ca4c7036f52e8759
Daniel Kanczuk <danielsussa@gmail.com>
2019-09-06 13:45:57 -0300

    add first adapter

--- src/main/java/io/lightflame/httpAdapters/HttpInAdapter.java
+++ src/main/java/io/lightflame/httpAdapters/HttpInAdapter.java
@@ -1,32 +1,34 @@
 package io.lightflame.httpAdapters;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
 import com.google.gson.Gson;
 
 import io.lightflame.functions.HttpInAdapterFunction;
-import io.netty.handler.codec.DecoderResult;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.util.CharsetUtil;
 
 /**
  * JsonUnmashallApaptor
  */
 public class HttpInAdapter {
     
-    public <E> HttpInAdapterFunction<E> jsonUnmarshall(Class<E> clazz) {
+    public <E> HttpInAdapterFunction<E> jsonUnmarshall(Class<E> clazz){
         Gson gson = new Gson();
         return (req) -> {
-            // StringBuilder buf = new StringBuilder();
-            // DecoderResult result = req.decoderResult();
-            // if (!result.isSuccess()) {
-            //     throw new Exception();
-            // }
-        
-            // buf.append(".. WITH DECODER FAILURE: ");
-            // buf.append(result.cause());
-            // buf.append("\r\n");
-
-            
-            // byte[] b =  s.writeBody();
-            // E obj = gson.fromJson(String.valueOf(b), clazz);
-            return null;
+            try {
+                HttpHeaders headers = req.headers();
+                if (headers.get(HttpHeaderNames.CONTENT_TYPE) != "application/json"){
+                    throw new IOException("content type not accepted");
+                }
+                String jsonStr = req.content().toString(CharsetUtil.UTF_8);
+                E obj = gson.fromJson(jsonStr, clazz);
+                return obj;
+            }catch(IOException e) {
+                throw new UncheckedIOException(e);
+            }
         };
     }
     
